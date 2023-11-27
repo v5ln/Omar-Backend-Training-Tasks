c5630ce240a485ea13578b67339f71f919228033
OMAR <68195919+v5ln@users.noreply.github.com>
2023-11-27 18:12:16 +0200

    Implement Simple Inventory Management
    - Created a basic inventory management system in C# with classes for Product and Inventory management.
    - Implemented core functionalities including adding, viewing, editing, deleting, and searching for products.
    - Introduced the Printer class to centralize and manage all user interface outputs, improving user interaction and code maintainability.
    - Enhanced error handling and user feedback within Inventory operations.
    - Organized main application flow in Program.cs for straightforward navigation and operation of inventory tasks.

---
+++ Simple Inventory Management System/Simple Inventory Management System/Simple Inventory Management System/Product.cs
@@ -0,0 +1,18 @@
+﻿public class Product
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+
+    public Product(string name, decimal price, int quantity)
+    {
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}, Price: {Price}, Quantity: {Quantity}";
+    }
+}
---
+++ Simple Inventory Management System/Simple Inventory Management System/Simple Inventory Management System/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Simple_Inventory_Management_System;
+
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        var inventory = new Inventory();
+
+        while (true)
+        {
+            Printer.Menu();
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Printer.AddName();
+                    string name = Console.ReadLine();
+                    Printer.AddPrice();
+                    decimal price = Convert.ToDecimal(Console.ReadLine());
+                    Printer.AddQuantity();
+                    int quantity = Convert.ToInt32(Console.ReadLine());
+                    inventory.AddProduct(name, price, quantity);
+                    break;
+                case 2:
+                    inventory.ViewProducts();
+                    break;
+                case 3:
+                    Printer.ViewExist();
+                    string existingName = Console.ReadLine();
+                    Printer.ViewName();
+                    string newName = Console.ReadLine();
+                    Printer.ViewPrice();
+                    decimal newPrice = Convert.ToDecimal(Console.ReadLine());
+                    Printer.ViewQuantity();
+                    int newQuantity = Convert.ToInt32(Console.ReadLine());
+                    inventory.EditProduct(existingName, newName, newPrice, newQuantity);
+                    break;
+                case 4:
+                    Printer.Delete();
+                    string deleteName = Console.ReadLine();
+                    inventory.DeleteProduct(deleteName);
+                    break;
+                case 5:
+                    Printer.Search();
+                    string searchName = Console.ReadLine();
+                    inventory.SearchProduct(searchName);
+                    break;
+                case 6:
+                    return;
+                default:
+                    Printer.InvaildInput();
+                    break;
+            }
+        }
+    }
+}
